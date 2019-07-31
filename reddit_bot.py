5bad6f0ab7f9e5037f667c92cf44fbb698403ecd
Chak Koppula <chak.koppula199@gmail.com>
2019-07-31 22:10:55 +0530

    file size is cut in half every half MB, now uses fnuction to comment on multiple subs

--- reddit_bot.py
+++ reddit_bot.py
@@ -7,6 +7,27 @@ import time
 """
 Data Source Credit: https://gitlab.insrt.uk/BarkingDog/barking-bot/blob/9ce097b0e7421770ef676e609e5fd7559d3cf96c/Data/facts.json
 """
+
+#function to comment on reddit posts
+def commentFacts(sub):
+	subreddit = reddit.subreddit(sub)
+	for submission in subreddit.hot(limit=50):
+
+		#check to see if the bot has already replied to this post
+		if submission.id not in replied_posts:
+
+			#get the title in lowercase
+			title = submission.title.lower()
+
+			#if title contains a keyword then add a comment
+			for key in facts.keys():
+				if key in title:
+					print("Replying to: " + submission.title + " in " + sub)
+					factReply = "Did you know " + random.choice(facts[key])
+					submission.reply(factReply)
+					replied_posts.append(submission.id)
+					break #exit out of loop after 1 comment, dont want to spam
+
 count = 0
 while True:
 	count += 1
@@ -30,30 +51,33 @@ while True:
 			replied_posts = list(filter(None, replied_posts)) #removes empty values
 			print(replied_posts)
 
-	#get top ten posts from the subreddit
-	subreddit = reddit.subreddit("AnimalsBeingBros")
-	for submission in subreddit.hot(limit=100):
+	commentFacts("AnimalsBeingBros")
+	commentFacts("AnimalsBeingDerps")
+	commentFacts("AnimalsBeingJerks")
+	commentFacts("funny")
+	commentFacts("science")
+	commentFacts("art")
+	commentFacts("gifs")
+	commentFacts("mildlyinteresting")
+	commentFacts("interestingasfuck")
+	commentFacts("tumblr")
+	commentFacts("UpliftingNews")
 
-		#check to see if the bot has already replied to this post
-		if submission.id not in replied_posts:
-
-			#get the title in lowercase
-			title = submission.title.lower()
-
-			#if title contains a keyword then add a comment
-			for key in facts.keys():
-				if key in title:
-					print("Replying to: " + submission.title)
-					factReply = "Beep Boop\nDid you know " + random.choice(facts[key])
-					#submission.reply(factReply)
-					replied_posts.append(submission.id)
-					break #exit out of loop after 1 comment, dont want to spam
+	#if file size is over half a MB then remove the oldest half of the codes as the are likely no longer in scope of top 50 in the subreddit
+	#this prevents the file size getting too big with long run times
+	if os.path.getsize("replied_posts.txt") > 500000:
+		length = len(replied_posts)
+		to_remove = int(length/2)
+		replied_posts = replied_posts[to_remove:]
 
 	#write updated list of seen posts to the file
 	with open("replied_posts.txt", "w") as file:
 		print(replied_posts)
 		for submission_id in replied_posts:
 			file.write(submission_id + "\n")
+
+	
+
 
 	#wait an hour before running again
 	time.sleep(3600) 
