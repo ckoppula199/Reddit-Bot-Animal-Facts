a76df57801d19fe0c568986647c2e8cadb04a454
Chak Koppula <chak.koppula199@gmail.com>
2019-07-31 19:59:22 +0530

    fixed some bugs where it would post on already commented posts

--- reddit_bot.py
+++ reddit_bot.py
@@ -9,23 +9,24 @@ with open("facts.json") as factData:
 	facts = json.load(factData)
 
 #create the reddit instance
-reddit = praw.Reddit("bot1") #note all my credentials are stored in the praw.ini file. See README for more details
+reddit = praw.Reddit("animalFactsBot") #note all my credentials are stored in the praw.ini file. See README for more details
 
 #check to see if file is present, if not create an empty list
-if not os.path.isfile("already_replied.txt"):
+if not os.path.isfile("replied_posts.txt"):
 	replied_posts = []
 
 #if reddit post ids are in the file, read them into a list
 else:
 
-	with open("already_replied.txt", "r") as file:
+	with open("replied_posts.txt", "r") as file:
 		replied_posts = file.read()
 		replied_posts = replied_posts.split("\n") #each id is on a new line
 		replied_posts = list(filter(None, replied_posts)) #removes empty values
+		print(replied_posts)
 
 #get top ten posts from the subreddit
 subreddit = reddit.subreddit("AnimalsBeingBros")
-for submission in subreddit.hot(limit=10):
+for submission in subreddit.hot(limit=100):
 
 	#check to see if the bot has already replied to this post
 	if submission.id not in replied_posts:
@@ -38,12 +39,13 @@ for submission in subreddit.hot(limit=10):
 			if key in title:
 				print("Replying to: " + submission.title)
 				factReply = "Beep Boop\nDid you know " + random.choice(facts[key])
-				submission.reply(factReply)
+				#submission.reply(factReply)
 				replied_posts.append(submission.id)
 				break #exit out of loop after 1 comment, dont want to spam
 
 #write updated list of seen posts to the file
 with open("replied_posts.txt", "w") as file:
+	print(replied_posts)
 	for submission_id in replied_posts:
 		file.write(submission_id + "\n")
 
